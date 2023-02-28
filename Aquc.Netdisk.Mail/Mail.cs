2f2c77ecb624ad1a4638d372d9fc5e737264ba97
aquamarine5 <50990424+aquamarine5@users.noreply.github.com>
2023-02-28 17:42:28 +0800

    +Mail

---
+++ Aquc.Netdisk.Mail/Mail.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Net.Mail;
+
+namespace Aquc.Netdisk.Mail
+{
+    public class Mail
+    {
+        private readonly ILogger<Mail> _logger;
+        public readonly string mail;
+        private readonly string key;
+        private readonly SmtpClient _smtpClient;
+        public Mail(ILogger<Mail> logger, string mail, string key,string host)
+        {
+            (_logger, this.mail, this.key) = (logger, mail, key);
+            _smtpClient = new SmtpClient
+            {
+                Port = 25,
+                Credentials = new NetworkCredential(mail, key),
+                Host = host
+            };
+        }
+            
+        public void Send(MailMessage mailMessage)
+        {
+            try
+            {
+                _smtpClient.Send(mailMessage);
+                _logger.LogDebug("Success send mail to {m}", string.Join(", ", mailMessage.To));
+            }
+            catch(SmtpException e) 
+            { 
+                _logger.LogError("Failed to send mail message: {name} {track}",e.Message,e.StackTrace);
+            }
+        }
+    }
+}
